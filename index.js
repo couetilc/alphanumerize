d60e4b28b32a7fa2c35935f83c5e7917d37a64a5
Connor Couetil <couetilc@gmail.com>
2020-03-07 21:44:13 -0500

    working implementation with 100% test coverage

---
+++ index.js
@@ -0,0 +1,29 @@
+const englishAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+function alphanumerize(numeral) {
+  let prevRemainder;
+  const processedAlphabet = englishAlphabet[englishAlphabet.length - 1] + englishAlphabet;
+  let alphabetized = '';
+  let dividend = numeral;
+  const divisor = englishAlphabet.length; // base of target number system
+  for (let n = 0; dividend > 0; n += 1) {
+    const remainder = dividend % divisor;
+    const quotient = Math.floor(dividend / divisor);
+    if (quotient === 0) {
+      if (prevRemainder === 0) {
+        // no more letters are needed
+        const offset = remainder - 1;
+        if (offset === 0) {
+          return alphabetized;
+        }
+        return processedAlphabet[offset] + alphabetized;
+      }
+    }
+    alphabetized = processedAlphabet[remainder] + alphabetized;
+    dividend = quotient;
+    prevRemainder = remainder;
+  }
+  return alphabetized;
+}
+
+export default alphanumerize;
--- test.js
+++ test.js
@@ -1,1 +1,28 @@
-const tap = require('tap');
+import tap from 'tap';
+import alphanumerize from '.';
+
+tap.test('alphabetizes numbers', async (t) => {
+  const testCases = Object.entries({
+    0: '',
+    1: 'a',
+    2: 'b',
+    3: 'c',
+    26: 'z',
+    27: 'aa',
+    28: 'ab',
+    52: 'az',
+    53: 'ba',
+    54: 'bb',
+    703: 'aaa',
+    731: 'abc',
+  });
+
+  testCases.forEach(([input, output]) => {
+    const alphabetized = alphanumerize(input);
+    t.equal(
+      alphabetized,
+      output,
+      `alphanumerize(${input}) should equal ${output}, NOT ${alphabetized}`,
+    );
+  });
+});
